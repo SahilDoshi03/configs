ddbb3488091e97ce5682e000251be85f52ea0874
Sahil Doshi <sahil.doshi@logixal.com>
2025-07-18 12:07:01 +0530

    add remap comments

--- nvim/lua/plugins/ctrlSF.lua
+++ nvim/lua/plugins/ctrlSF.lua
@@ -3,14 +3,11 @@ return {
   dependencies = { 'wincent/ferret' }, -- Optional but improves search
   config = function()
     vim.g.ctrlsf_default_root = 'project'
-    vim.g.ctrlsf_auto_close = {
-      at_exit = 0,
-      at_entry = 0,
-    }
+    vim.g.ctrlsf_auto_close = 0
+    vim.g.ctrlsf_auto_focus = 1
     vim.g.ctrlsf_position = 'left'
-    vim.g.ctrlsf_auto_focus = {
-      at_open = true,
-    }
+
+    vim.keymap.set('n', '<C-f>', ':CtrlSF ', { noremap = true })
   end
 }
 
--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,13 +1,11 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+-- push selection up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- select entire buffer
 vim.keymap.set("n", "<C-a>", "ggVG")
--- select entire line
-vim.keymap.set("n", "<C-l>", "^v$")
-
+-- select line from content start to end
+vim.keymap.set("n", "<C-l>", "^vg_")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
@@ -18,32 +16,50 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- keep cursor in the center when searching
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
+-- replace selection without yanking the selection
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
+-- delete without yankind the deletion
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set({ "i", "o", "v", "n", "c", "t" }, "<C-c>", "<Esc>")
+-- copy to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
+-- copy line to clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- avoid going to :Ex
 vim.keymap.set("n", "Q", "<nop>")
+
+-- format buffer
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- navigate in quickfix list
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
+-- navigate in location list
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- global find and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- source vim
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
 
+-- hide highlights
 vim.keymap.set("n", "<leader>rh", ":noh<CR>")
+
+-- resize pane
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize +2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize -2<CR>")
+
 
 -- copy the diagnostic messages (error, warnings) etc
 vim.keymap.set('n', '<leader>e', function()
@@ -55,4 +71,3 @@ vim.keymap.set('n', '<leader>e', function()
     print("No diagnostic on this line")
   end
 end)
-
